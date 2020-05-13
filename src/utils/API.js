8371906e90ae3817a640948edb2f7f488bfb31d4
maangeloatienza <maangeloatienza@gmail.com>
2020-05-14 00:10:03 +0800

    prod api

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { getToken } from './Commons';
 
 export default axios.create({
-    // baseURL: "http://54.179.163.208:1111/api/v1/",
-    baseURL: "http://localhost:8000/api/v1/",
+    baseURL: "http://54.179.163.208:1111/api/v1/",
+    // baseURL: "http://localhost:8000/api/v1/",
     headers: {
         'Authorization': `${getToken()}`,
         // "Content-type": "application/json"
